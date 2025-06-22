88c057856178704abfc7d6fe04a3f55310cfaa68
Brett Cunningham <bwcunninghamii@gmail.com>
2025-06-22 13:33:12 -0400

    wip(test): for adding a jwt filter on a protected route

---
+++ src/test/java/com/altruistic_software_development/ally_bill_tracker_backend/JwtAuthenticationFilterTest.java
@@ -0,0 +1,59 @@
+package com.altruistic_software_development.ally_bill_tracker_backend;
+
+import com.altruistic_software_development.ally_bill_tracker_backend.model.Role;
+import com.altruistic_software_development.ally_bill_tracker_backend.model.User;
+import com.altruistic_software_development.ally_bill_tracker_backend.repository.UserRepository;
+import com.altruistic_software_development.ally_bill_tracker_backend.service.JwtService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+import java.util.Set;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class JwtAuthenticationFilterTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private JwtService jwtService;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @BeforeEach
+    void setUp() {
+        userRepository.deleteAll();
+
+        User user = User.builder()
+                .email("test@example.com")
+                .password(passwordEncoder.encode("password"))
+                .roles(Set.of(Role.USER))
+                .build();
+        userRepository.save(user);
+    }
+
+    @Test
+    void shouldAuthorizeRequestWithValidToken() throws Exception {
+        User user = userRepository.findByEmail("test@example.com").get();
+        String token = jwtService.generateToken(user.getId(), user.getRoles());
+
+        mockMvc.perform(
+                get("/api/v1/protected-route")
+                    .header("Authorization", "Bearer " + token)
+                )
+                .andExpect(status().isOk());
+    }
+}
