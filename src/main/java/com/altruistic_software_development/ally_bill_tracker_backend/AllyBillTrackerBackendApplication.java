a228b2399941041ee6365faaad81f0a9230acbcc
Brett Cunningham <brettcunningham@Big-Bretts-MacBook-Pro.local>
2025-06-22 09:31:27 -0400

    completed(all): second commit

---
+++ src/main/java/com/altruistic_software_development/ally_bill_tracker_backend/AllyBillTrackerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.altruistic_software_development.ally_bill_tracker_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllyBillTrackerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllyBillTrackerBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/altruistic_software_development/ally_bill_tracker_backend/AllyBillTrackerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.altruistic_software_development.ally_bill_tracker_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllyBillTrackerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
